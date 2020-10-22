d21e286205612f389194de7755fb009022723a42
Richard W. L. Brehmer <richardbrehmer@Richards-MacBook-Pro.local>
2020-10-22 19:51:57 -0200

    backend finalizado

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,9 +2,19 @@ import { Request, Response } from 'express'; // Necessário
 import { getRepository } from "typeorm";
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup'; // Usado * quando o export defaul la na origem.
 
 export default {
     async create(req: Request, res: Response){
+        const { 
+            name, 
+            latitude, 
+            longitude, 
+            about, 
+            instructions, 
+            opening_hours,
+            open_on_weekends,
+        } = req.body;
 
         const orphanagesRepository = getRepository(Orphanage);
         
@@ -14,14 +24,37 @@ export default {
             return { path: image.filename };
         });
 
-        const { name, latitude, longitude, about, instructions, 
-            opening_hours,open_on_weekends,
-        } = req.body;
+        const data = {
+            name, 
+            latitude, 
+            longitude, 
+            about, 
+            instructions, 
+            opening_hours,
+            open_on_weekends, 
+            images,
+        };
 
-        const orphanage = orphanagesRepository.create({
-            name, latitude, longitude, about, instructions, 
-            opening_hours,open_on_weekends, images,
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            ),
         });
+
+        await schema.validate(data, {
+            abortEarly: false, // deixar rodar todos os erros.
+        });
+
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
     
